15651062d909b811d34aa55cfbb5c30bc821a196
Hatice Kilicer <hatice.klcr0@gmail.com>
2026-07-21 12:03:10 +0300

    Initialize Spring Boot project

---
+++ src/main/java/com/hatice/loginsight/LogInsightApplication.java
@@ -0,0 +1,13 @@
+package com.hatice.loginsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogInsightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogInsightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hatice/loginsight/LogInsightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hatice.loginsight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogInsightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
